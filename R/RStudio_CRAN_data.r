8c3df7d21384df35b88887c1f564d2b4646ed670
Tal Galili <tal.galili@gmail.com>
2013-06-12 09:05:35 +0300

    download_RStudio_CRAN_data no longer breaks if a wrong time range is provided.
    Also added different defaults to START and END dates (for today and 5 days before today)

--- R/RStudio_CRAN_data.r
+++ R/RStudio_CRAN_data.r
@@ -9,9 +9,13 @@
 #' This function downlaod these files based on the code from the downlaod page (\url{http://cran-logs.rstudio.com/}) into a temporary folder.
 #' @details
 #' RStudio maintains its own CRAN mirror, \url{http://cran.rstudio.com} and offers its log files.
-#' @param START A character string of the START date for files to be downloaded. The date format is "YYYY-MM-DD".
-#' @param END A character string of the END date for files to be downloaded. The date format is "YYYY-MM-DD".
+#' @param START the defaults is 5 days before today. A character string of the START date for files to be downloaded. The date format is "YYYY-MM-DD".
+#' @param END the defaults is today. A character string of the END date for files to be downloaded. 
+#' The date format is "YYYY-MM-DD".
 #' @param log_folder the folder into which we would like the files to be downloaded to. Default is the temporary folder picked by \link{tempdir}.
+#' @param trunc_END_date_to_today default is TRUE. Makes sure that if END date is later then today,
+#'  the END date will be change to today
+#'  (since otherwise, we will only get many 404 errors)
 #' @param ... not in use.
 #' @return Returns the value of log_folder.
 #' @seealso \link{download_RStudio_CRAN_data}, \link{read_RStudio_CRAN_data},\link{barplot_package_users_per_day}
@@ -19,16 +23,20 @@
 #' \dontrun{
 #' # The first two functions might take a good deal of time to run (depending on the date range)
 #' RStudio_CRAN_data_folder <- download_RStudio_CRAN_data(START = '2013-04-02', END = '2013-04-05') # around the time R 3.0.0 was released
+#' # RStudio_CRAN_data_folder <- download_RStudio_CRAN_data()
 #' my_RStudio_CRAN_data <- read_RStudio_CRAN_data(RStudio_CRAN_data_folder)
 #' 
 #' # barplots: (more functions can easily be added in the future)
 #' barplot_package_users_per_day("installr", my_RStudio_CRAN_data)
 #' barplot_package_users_per_day("plyr", my_RStudio_CRAN_data)
 #' }
-download_RStudio_CRAN_data <- function(START = '2013-04-02', END = '2013-04-20', log_folder = tempdir(),...) {
+download_RStudio_CRAN_data <- function(START = as.Date(Sys.time())-5, END = as.Date(Sys.time()), log_folder = tempdir(), trunc_END_date_to_today = TRUE,...) {
    # Here's an easy way to get all the URLs in R
    START <- as.Date(START)
    END <- as.Date(END)
+   
+   # If END date is much further away than today (based on system definitions), it should be tka
+   if((END > as.Date(Sys.time())+1) & trunc_END_date_to_today) END <- as.Date(Sys.time())+1 # the +1 is just for the case of a difference in times between the computer and the RStudio server.
    
    all_days <- seq(START, END, by = 'day')
    
@@ -43,7 +51,7 @@ download_RStudio_CRAN_data <- function(START = '2013-04-02', END = '2013-04-20',
    # download files
    for(i in seq_along(urls)) {
       zip_filename <- file.path(log_folder, file.name.from.url(urls[i]))
-      download.file(urls[i], destfile=zip_filename, mode = 'wb')   
+      tryCatch(download.file(urls[i], destfile=zip_filename, mode = 'wb'), error = function(e) e)
    }
 
    return(log_folder)
@@ -52,6 +60,7 @@ download_RStudio_CRAN_data <- function(START = '2013-04-02', END = '2013-04-20',
 
 # http://www.r-bloggers.com/where-is-the-r-activity/
 # source:  http://psychwire.wordpress.com/2011/06/03/merge-all-files-in-a-directory-using-r-into-a-single-dataframe/
+
 
 
 #' @title Reads RStudio CRAN mirror data files from a folder
@@ -85,6 +94,10 @@ read_RStudio_CRAN_data <- function(log_folder = tempdir(),...) {
                                                                     header=TRUE, sep=",", stringsAsFactors = FALSE, check.names = FALSE)}))
    return(dataset)
 }
+
+
+
+# system.time(my_RStudio_CRAN_data <- read_RStudio_CRAN_data(RStudio_CRAN_data_folder))
 
 
 
