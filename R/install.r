abd12ab12b87e3d3177d000d4c0ab3a1d3295011
Tal Galili <tal.galili@gmail.com>
2013-08-21 23:39:19 +0300

    Fix question in require2

--- R/install.r
+++ R/install.r
@@ -1091,7 +1091,8 @@ source.https <- function(URL,..., remove_r_file = T) {
 require2 <- function(package, ask= TRUE, ...) {
    package <- as.character(substitute(package))
    if(!suppressWarnings(require(package=package, character.only = TRUE))) {
-      install_package <- ask.user.yn.question(paste("Package ",package, " is not installed. Should it be installed?"))
+      install_package <- ask.user.yn.question(paste("Package ",package, 
+                                                    " is not installed. Do you want to install it now?"))
       if(install_package) install.packages(pkgs=package)
    }
    require(package=package, character.only = TRUE)
