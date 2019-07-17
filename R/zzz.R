7c0881a183352c633cc70181a5c56d4daa9c902f
Tal Galili <tal.galili@gmail.com>
2019-07-17 23:43:15 +0300

    minor

--- R/zzz.R
+++ R/zzz.R
@@ -216,6 +216,6 @@ installrWelcomeMessage <- function(){
 #                 Thanks to: http://stackoverflow.com/questions/10017702/r-cmd-check-options-for-more-rigorous-testing-2-15-0
 # file.copy("NEWS", "NEWS.md", overwrite = TRUE)
 # shell('git log --graph --stat --date=short --pretty=format:"%ad(%an) %s |%h" > ChangeLog', intern = TRUE)
-# devtools::build_win(version = "R-devel")
+# devtools::check_win_devel()
 # release(check = TRUE)
 
